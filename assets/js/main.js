aafb62e84fbed795c98ae810508aa0bb51d3ec7c
Ulianna0309 <ulianna0309@gmail.com>
2021-04-14 14:26:22 +0300

    create libs

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,11 +109,16 @@ $(document).ready(function () {
 }());
 $('.personal-container__items-mobile').slick({
     dots: true,
-    infinite: true,
-    speed: 300,
+    speed: 500,
     slidesToShow: 1,
     adaptiveHeight: true
 });
+
+$('.actions .button.big').on('click', function(e){
+    e.preventDefault();
+    $(this).text( $(this).text() == "Continue Reading" ? "Show Less" : "Continue Reading" );
+    $(this).closest('article.post').find('.hidden-content').slideToggle()
+})
 
 
 
