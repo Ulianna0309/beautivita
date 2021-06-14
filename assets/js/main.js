9753c1237b69d6f305b7ed53c45070ead0f03ba3
Ulianna0309 <ulianna0309@gmail.com>
2021-06-14 21:22:47 +0300

    fix scroll to the pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,59 @@
-/*scroll page
-(function () {
+(function() // Code in a function to create an isolate scope
+{
+    var speed = 1000;
+    var moving_frequency = 15; // Affects performance !
+    var links = document.getElementsByTagName('a');
+    var href;
+    for(var i=0; i<links.length; i++)
+    {
+        href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
+        if(href !== null && href.length > 1 && href.substr(0, 1) == '#')
+        {
+            links[i].onclick = function()
+            {
+                var element;
+                var href = this.attributes.href.nodeValue.toString();
+                if(element = document.getElementById(href.substr(1)))
+                {
+                    var hop_count = speed/moving_frequency
+                    var getScrollTopDocumentAtBegin = getScrollTopDocument();
+                    var gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
 
-    const smoothScroll = function (targetEl, duration) {
-        const headerElHeight =  document.querySelector('.header').clientHeight;
-        let target = document.querySelector(targetEl);
-        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-        let startPosition = window.pageYOffset;
-        let startTime = null;
+                    for(var i = 1; i <= hop_count; i++)
+                    {
+                        (function()
+                        {
+                            var hop_top_position = gap*i;
+                            setTimeout(function(){  window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency*i);
+                        })();
+                    }
+                }
 
-        const ease = function(t,b,c,d) {
-            t /= d / 2;
-            if (t < 1) return c / 2 * t * t + b;
-            t--;
-            return -c / 2 * (t * (t - 2) - 1) + b;
-        };
+                return false;
+            };
+        }
+    }
 
-        const animation = function(currentTime){
-            if (startTime === null) startTime = currentTime;
-            const timeElapsed = currentTime - startTime;
-            const run = ease(timeElapsed, startPosition, targetPosition, duration);
-            window.scrollTo(0,run);
-            if (timeElapsed < duration) requestAnimationFrame(animation);
-        };
-        requestAnimationFrame(animation);
+    var getScrollTopElement =  function (e)
+    {
+        var top = 0;
 
+        while (e.offsetParent != undefined && e.offsetParent != null)
+        {
+            top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+            e = e.offsetParent;
+        }
+
+        return top;
     };
 
-    const scrollTo = function () {
-        const links = document.querySelectorAll('.js-scroll');
-        links.forEach(each => {
-            each.addEventListener('click', function () {
-                const currentTarget = this.getAttribute('href');
-                smoothScroll(currentTarget, 1000);
-            });
-        });
+    var getScrollTopDocument = function()
+    {
+        return document.documentElement.scrollTop + document.body.scrollTop;
     };
-    scrollTo();
-}());*/
+})();
+
+
 
 
 $(document).ready(function() {
